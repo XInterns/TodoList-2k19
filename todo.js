ac6c8199409450a648ac8a24d39822d11c0dba52
premj90 <premj90@gmail.com>
2019-06-07 17:21:32 +0530

    JS Added Part 2

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-var todo_array = [
+var todo = [
     { task: "Eat", isCompleted: true },
     { task: "Sleep", isCompleted:false },
     { task: "Rave", isCompleted:false },
@@ -9,26 +9,35 @@ var todo_array = [
    
    const createTodoListHTML = ({ task, isCompleted }) => {
     data_id++;
-    return `<li data-id=${data_id} class=${isCompleted ? 'completed' : ''}><div class='view'><input class='toggle' type='checkbox' checked=''><label>${task}</label><button class="destroy"></button></div></li>`
+    return `<li data-id=${data_id} class=${isCompleted ? 'completed' : ''}><div class='view'><input class='toggle' type='checkbox' ${isCompleted? 'checked':''}><label>${task}</label><button class="destroy"></button></div></li>`
    }
    
-   const refreshTodoList = (todo_array) => {
-    const todosHTML = todo_array.map((todo) => createTodoListHTML(todo));
+   const refreshTodoList = (todo) => {
+    const todosHTML = todo.map((todo) => createTodoListHTML(todo));
     document.getElementById('display-todos').innerHTML = todosHTML.join('');
+   }
+   const activeCount = (todo) =>   
+   {
+       var c = 0;
+       for(var i=0;i<todo.length;i++)
+       {
+        if(todo[i].isCompleted === false){
+            c++;
+        }
+       }
+       
+       document.getElementById('act-disp').innerHTML= c + " "+ "items left";
+    
    }
 
    const getTodos = (showCompleted) => {
     if (showCompleted == undefined) {
-      displayTodos = todo_array;
+      displayTodos = todo;
     }
     else {
-      displayTodos = todo_array.filter(({ isCompleted }) => isCompleted === showCompleted);
+      displayTodos = todo.filter(({ isCompleted }) => isCompleted === showCompleted);
     }
     refreshTodoList(displayTodos)
    }
 
-
-
-
-
-
+   activeCount(todo);
