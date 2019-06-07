eb35aca65c85207999ca2f78ca11571c3445ef43
rishmish <rishabhismishra@gmail.com>
2019-06-07 15:33:13 +0530

    display functionalities added in js

--- function.js
+++ function.js
@@ -1,21 +1,28 @@
-function getAllActive(arr){
-    var temp_arr=[]
-    var len=arr.length
-    for(i=0;i<len;i++){
-        if(!array[i]['completed']){
-            temp_arr.push(arrayt[i])
-        }
-    }
-    return temp_arr
+var todo_array = [
+  { task: "Go Meet Friends", isCompleted: true }, 
+  { task: "Finish Black Mirror Season 5", isCompleted:false },
+  { task: "Complete JavaScript course", isCompleted:false }, 
+  { task: "Complete HTML/CSS course", isCompleted:false },
+  { task: "Finish Dororo New Episode", isCompleted: true },];
+
+var data_id = 0;
+
+const createTodoListHTML = ({ task, isCompleted }) => {
+  data_id++;
+  return `<li data-id=${data_id} class=${isCompleted ? 'completed' : ''}><div class='view'><input class='toggle' type='checkbox' ${isCompleted ? 'checked' : ''}><label>${task}</label><button class="destroy"></button></div></li>`
 }
 
-function getAllCompleted(arr){
-    var temp_arr=[]
-    var len=arr.length
-    for(i=0;i<len;i++){
-        if(array[i]['completed']){
-            temp_arr.push(arrayt[i])
-        }
-    }
-    return temp_arr
+const refreshTodoList = (todo_array) => {
+  const todosHTML = todo_array.map((todo) => createTodoListHTML(todo));
+  document.getElementById('display-todos').innerHTML = todosHTML.join('');
 }
+const getTodos = (showCompleted) => {
+  if (showCompleted == undefined) {
+    displayTodos = todo_array;
+  }
+  else {
+    displayTodos = todo_array.filter(({ isCompleted }) => isCompleted === showCompleted);
+  }
+  refreshTodoList(displayTodos)
+}
+
