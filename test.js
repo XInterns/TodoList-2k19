e131d788d42ea6c32cc5d5c5214f9b99f1d11511
Chhavi-97 <500053339@stu.upes.ac.in>
2019-06-10 18:50:41 +0530

    updated

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 let displayTodos = [];
 
-const todoList = [
+var todoList = [
 
-    { name: 'Wake Up', isCompleted: false }, { name: 'Get Ready for Office', isCompleted: false }, { name: 'Take Metro', isCompleted: false }, { name: 'Get Off Metro', isCompleted: true }, { name: 'Take Shuttle', isCompleted: false }, { name: 'Reach Office', isCompleted: false }];
+    { name: 'Wake Up', isCompleted: false }, { name: 'Get Ready for Office', isCompleted: false }, { name: 'Take Metro', isCompleted: false }, { name: 'Get Off Metro', isCompleted: false }, { name: 'Take Shuttle', isCompleted: false }, { name: 'Reach Office', isCompleted: false }];
 var x;
 const getTodos = (showCompleted) => {
 
@@ -24,7 +24,7 @@ const createTodoFromTemplate = ({ name, isCompleted },index) => {
     <label for="check-checkbox"><i onclick="checkboxtoggle(this,${index})" class='${isCompleted ? 'far fa-check-circle' : 'fas fa-circle'}'></i></label>
     <input  id="check-checkbox" class="hide" type="checkbox"/>
     <label>${name}</label>
-    <button class="remove"></button></li>`;
+    <button class="remove" onclick="removeCompletedTasks(${index})"></button></li>`;
 }
 
 
@@ -55,26 +55,30 @@ const displayRemainingTask = (todoList) => {
 var input = document.querySelector("input[type = 'text']");
 var ul = document.querySelector("ul");
 
-input.addEventListener("keypress", function (keyPressed) {
-    if (keyPressed.which === 13) {
-        var li = document.createElement("li");
-        var spanElement = document.createElement("span");
-        var icon = document.createElement("i");
+// input.addEventListener("keypress", function (keyPressed) {
+//     if (keyPressed.which === 13) {
+//         var li = document.createElement("li");
+//         var spanElement = document.createElement("span");
+//         var icon = document.createElement("i");
 
-        var newTodo = this.value;
-        var newtask = { name: newTodo, isCompleted: false };
-        //this.value = " " ;
-        todoList.push(newtask);
+//         var newTodo = this.value;
+//         var newtask = { name: newTodo, isCompleted: false };
+//         todoList.push(newtask);
 
 
-        icon.classList.add('fas', 'fa-circle');
-        spanElement.append(icon);
-        ul.appendChild(li).append(spanElement, newTodo);
-        refreshTodosList(todoList);
+//         icon.classList.add('fas', 'fa-circle');
+//         spanElement.append(icon);
+//         ul.appendChild(li).append(spanElement, newTodo);
+//         refreshTodosList(todoList);
 
-    }
+//     }
 
-});
+// });
+
+
+
+
+
 function checkboxtoggle(element,index) {
     if (element.classList == "fas fa-circle") 
     {
@@ -93,11 +97,68 @@ function toggletodo(todoList,i){
     todoList[i].isCompleted=(todoList[i].isCompleted)?false:true;
 }
 
-// function removeCompletedTasks(todoList,index){
+// ul.addEventListener('click',removeTasks);
 
-//     todoList[i].
-
+// function removeTasks(e){
+    
+//         if(e.target.classList.contains('remove')){
+//        e.target.parentElement.remove();
+       
+//         }
+   
 // }
+
+function removeCompletedTasks(index){
+    todoList.splice(index,1);
+    refreshTodosList(todoList);
+}
+
+// function clearAllCompleted(){
+
+// for (x of todoList) {
+//         var activeTaskList = todoList.filter(({ isCompleted }) => isCompleted === true);
+// // console.log(activeTaskList)
+//     // if(x.isCompleted === true) {
+//         todoList.filter( function (el) {
+//             return console.log(!activeTaskList.includes(el)) 
+//         })
+//     // }
+// }
+
+    // var i=todoList.length;
+         
+    // for(y=0;y<todoList.length;y++)
+    // { 
+    //     if(todoList[y].isCompleted===true)
+    //     {
+    //         todoList.splice(y,1);
+                  
+                  
+    //               //console.log(i);
+    //             //   y++;
+    //          }
+        
+    //          refreshTodosList(todoList);
+      
+        
+    // }
+}
+//     var activeTaskList = todoList.filter(({ isCompleted }) => isCompleted === false);
+//     for(i=0;i<todoList.length;i++){
+//         todoList[i]=activeTaskList[i];
+//     }
+//     refreshTodosList(todoList);
+
+    
+// }
+
+// function  clearAllCompleted(){
+    
+// }
+
+
+
+
 // }x.classList.toggle("fa-check-circle");}
 // <i class="fas fa-circle"></i>
 
